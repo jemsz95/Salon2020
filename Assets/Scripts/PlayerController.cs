d48db487cc54c25d4a8cc020ba11a940ae9fe9f0
Javier Meza <javenmeza@gmail.com>
2016-09-29 12:13:55 -0500

    Interaccion en VR

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int Speed;
+
+	private Vector3 target;
+
+	public void Move(Vector3 pos) {
+		target = pos;
+	}
+
+	// Use this for initialization
+	void Start () {
+		//Tell the nav system the player to move
+		NavPoint.Player = this;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
