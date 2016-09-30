9a814a1387bd94f8d7a09a5a74d4eb0a0b8e513a
AntonioVT <tony-vt@hotmail.com>
2016-09-30 11:46:02 -0500

    Workaround

---
+++ Assets/NavSceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NavSceneLoader : MonoBehaviour {
+
+    public string sSceneName;
+    public GameObject canvasFade;
+
+	void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        canvasFade.SetActive(true);
+        yield return new WaitForSeconds(1.0f);
+        SceneManager.LoadScene(sSceneName);
+    }
+
+
+}
